1772b268efc7d383c1bd964430c503e873f0a542
abhinav <abhinav999k@gmail.com>
2023-01-31 15:26:01 +0530

    backend

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "azurerm" {
+    subscription_id      = "46aa4e0d-9dd3-4108-85e4-a2aa5e6a1443"
+    resource_group_name  = "atfadds"
+    storage_account_name = "atfadds"
+    container_name       = "tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
